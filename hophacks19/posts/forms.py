f39b170698e812ff1fd6519d08920cb5f99eced0
jzwang43 <jzwang43@gmail.com>
2019-02-16 12:05:01 -0500

    adding functionalities

--- hophacks19/posts/forms.py
+++ hophacks19/posts/forms.py
@@ -4,5 +4,10 @@ from . import models
 
 class PostForm(forms.ModelForm):
     class Meta:
-        fields = ("message", "amount")
+        fields = ["message", "amount", "disaster"]
         model = models.Post
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["disaster"].queryset = models.Disaster.objects.all()
+
--- hophacks19/posts/models.py
+++ hophacks19/posts/models.py
@@ -2,12 +2,26 @@ from django.db import models
 from django.utils import timezone
 from django.urls import reverse
 
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Disaster(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    description = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
-    user = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    disaster = models.ForeignKey(Disaster, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     received_at = models.DateTimeField(auto_now=True)
-    message = models.TextField()
+    message = models.CharField(max_length=255)
     amount = models.IntegerField()
 
     def __str__(self):
@@ -16,3 +30,4 @@ class Post(models.Model):
     class Meta:
         ordering = ["-created_at"]
         unique_together = ["user", "message"]
+
