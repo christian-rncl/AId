588a66adfffeda78e8dfa112d903fd4e145e4de1
Christian Roncal <cjl.roncal@gmail.com>
2019-02-17 05:34:58 -0500

    finished verification

--- api/api.py
+++ api/api.py
@@ -17,15 +17,11 @@ def register_usr(phone, r):
   for field in signup_order:
     if fields[field] == "":
       if field == 'first_name':
-        return "Enter a name everyone will see"
+        return "Enter a name everyone will see 'name:<username>'"
 
-def ensure_name(phone, name):
-  r = requests.get(CORE_API+phone).text
-  fields = get_fields(r)
-  if fields['first_name'] == '':
+def register_name(phone, name):
     print(requests.get(CORE_API + phone + "/first/" + name))
     return True
-  return False
 
 def addPost(phone, body):
   print(CORE_API + phone + "/post/" +body)
@@ -40,12 +36,19 @@ def addDisaster(phone, state):
   if(state.upper() in disaster_areas.keys()):
     requests.get(CORE_API + phone + '/post/disaster/' + str(disaster_areas[state.upper()]))
 
+def notifyVerify(r):
+  fields = json.loads(r)[0]['fields']
+  msg = "Please image verify by texting us an image of your purchase and 'verify'"
+  send_msg(fields['username'], msg)
+
 @app.route('/notifyuser', methods=['GET', 'POST'])
 def notifyUser():
   req = request.form.to_dict(flat=False)
   # print('REQ: ', req)
+  # print(req['category'])
   msg = "You've been donated: $" + req['amount'][0] + " for: " + req['category'][0] + " pay with pin: " + str(req['pin'][0])
   send_msg(req['phone'][0], msg)
+  return 'okay'
   
 
 @app.route("/sms", methods=['GET', 'POST'])
@@ -58,24 +61,29 @@ def incoming_sms():
     body = "".join(request.values.get('Body', None).lower().split())
     phonenum = request.values.get('From', None)[2:]
     from_state = request.values.get('FromState', None)
-    #from_country = request.values.get('FromCountry', None)
+    print(body[:6])
 
     if(body == "helpme"):
       r = requests.get(CORE_API+phonenum).text
       msg = register_usr(phonenum, r)
       resp.message(msg)
     
+    elif(body[:4] == 'name'):
+        msg = "Okay, " + body[5:] + ", text us 'newpost: <what you need (not how much)>'"
+        register_name(phonenum, body[5:])
+        resp.message(msg)
+    
     elif(body[:7] == "newpost"):
       texts = orig_body.split(':')
       addPost(phonenum, texts[1])
-      msg = "okay, I got your post, how much do you need? text us 'amount: <number>'"
+      msg = "okay, I got your post, how much $ do you need? text us 'amount: <number>'"
       addDisaster(phonenum, from_state) 
       resp.message(msg)
 
     elif(body[:6] == "amount"):
       amt = "".join(body[7:].split())
       requests.get(CORE_API + phonenum + "/post/amount/" + amt)
-      resp.message("Thanks! What's this for? text 'for:<food, bottled_water>'")
+      resp.message("Thanks! What category does this fall under? text 'for:<food, bottled_water>'")
 
     elif(body[:3] == "for"):
       print(CORE_API + phonenum + "/post/cat/" + body[4:])
@@ -85,7 +93,8 @@ def incoming_sms():
 
     elif(body[:7] == "payment"):
       pin = body.split(":")[1]
-      requests.get(CORE_API + phonenum + "/transaction/" + pin)
+      r = requests.get(CORE_API + phonenum + "/transaction/" + pin).text
+      notifyVerify(r)
       msg = "You just got paid! Check your account"
       resp.message(msg)
 
@@ -95,32 +104,17 @@ def incoming_sms():
           msg = requests.get(image_url).content
           pred = cv_verify.predict(msg, PROJECT_ID, MODEL_ID) 
 
-          prediction = pred.payload[0].display_name
+          label = pred.payload[0].display_name
 
-          msg ="You verified a purchase for " + prediction + "!"
+          requests.get(CORE_API + phonenum + "/user/" + label)
+          msg ="You verified with a picture of " + label
           resp.message(msg)
       else:
           resp.message("You need to verify with a picture!")
 
 
     else:
-      if ensure_name(phonenum, body):
-        msg = "Okay, " + body + ", text us 'newpost: <what you need (not how much)>'"
-        resp.message(msg)
-      else:
-        resp.message("Emergency? Make a request by sending 'help me'")
-
-    # if request.values['NumMedia'] != '0':
-
-    #     # Use the message SID as a filename.
-    #     image_url = request.values['MediaUrl0']
-    #     msg = requests.get(image_url).content
-    #     pred = cv_verify.predict(msg, PROJECT_ID, MODEL_ID) 
-
-    #     print(pred.payload[0])
-    #     resp.message(pred.payload[0].display_name)
-    # else:
-    #     resp.message("Try sending a picture message.")
+      resp.message("Emergency? Make a request by sending 'help me'")
 
     return str(resp)
 
